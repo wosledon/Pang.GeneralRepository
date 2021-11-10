c437e4e31be51d06f72d6d7c1a45975b89aa7863
DonPangPang <powertle@outlook.com>
2021-11-10 20:50:52 +0800

    update

---
+++ src/Pang.GeneralRepository.Core/Model/ReturnDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pang.GeneralRepository.Core.Model
+{
+    /// <summary>
+    /// 返回实体
+    /// </summary>
+    public class ReturnDto
+    {
+        /// <summary>
+        /// 状态码
+        /// </summary>
+        public int Code { get; set; }
+
+        /// <summary>
+        /// 返回消息
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 数据
+        /// </summary>
+        public Object Data { get; set; }
+    }
+}
